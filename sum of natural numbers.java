37fb785420b1b84d72d1e2efb2dc899af84aab84
AKSHAYA.G <akshaya26.inn@gmail.com>
2025-12-22 22:21:23 +0530

    Add files via upload

---
+++ sum of natural numbers.java
@@ -0,0 +1,13 @@
+public class iterative {
+    public static void main(String[] args)
+    {
+        int n = 15;
+        int sum = 0;
+
+        for(int i=0;i<=n;i++) {
+            sum = sum + i;
+        }
+        System.out.println("Sum of n number is:" +sum);
+        }
+    }
+
