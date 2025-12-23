9002d7793f0a156cca4e9850a3b78980741bfc14
AKSHAYA.G <akshaya26.inn@gmail.com>
2025-12-23 18:47:11 +0530

    Add files via upload

---
+++ while_loop.java
@@ -0,0 +1,17 @@
+public class while_loop {
+    public static void main(String[] args)
+    {
+        System.out.println("Using for loop");
+        for(int i=1;i<10;i++)
+        System.out.println(i);
+
+        System.out.println("Using do while loop");
+        int j = 1;
+        do
+            {
+                System.out.println(j);
+                j++;
+            } while(j<10);
+        }
+    }
+
