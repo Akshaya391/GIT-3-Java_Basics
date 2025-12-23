1f8bfa52a09219cefd430562665cfff2fce6bb1b
AKSHAYA.G <akshaya26.inn@gmail.com>
2025-12-23 18:10:39 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,15 @@
+
+    public class factorial
+    {
+        public static void main(String[] args)
+        {
+            int n = 5;
+            int factorial = 1;
+            for(int i=1;i<=n;i++)
+            {
+                factorial*=i;
+            }
+            System.out.println("Factorial of number is:" +factorial);
+        }
+    }
+
